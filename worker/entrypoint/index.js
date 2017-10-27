93e88bc1f88c61dab3bb3f6278a3d6c49713c6e1
Fred-el-Jolo <guillaume.frederic@gmail.com>
2017-10-27 16:03:02 +0200

    Update coding rules

--- worker/entrypoint/index.js
+++ worker/entrypoint/index.js
@@ -1,14 +1,15 @@
-import NATS from 'nats'
-const nats = NATS.connect('nats://ruser:T0pS3cr3t@nats:4222')
+import NATS from 'nats';
+
+const nats = NATS.connect('nats://ruser:T0pS3cr3t@nats:4222');
 // const nats = NATS.connect()
 
-export function initialize () {
-  console.log('Entered in init function')
-  // Simple Publisher
-  nats.publish('foo', 'Hello World!')
+export function initialize() {
+    console.log('Entered in init function');
+    // Simple Publisher
+    nats.publish('foo', 'Hello World!');
 
-  // Simple Subscriber
-  nats.subscribe('foo', function (msg) {
-    console.log('Received a message: ' + msg)
-  })
+    // Simple Subscriber
+    nats.subscribe('foo', (msg) => {
+        console.log(`Received a message: ${msg}`);
+    });
 }
