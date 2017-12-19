019ffa52be24bb50eddd3ed1e628cf172300acb9
Fred-el-Jolo <guillaume.frederic@gmail.com>
2017-12-19 16:17:25 +0100

    Refactor promises usage

--- vendor/serializer/protobuf.js
+++ vendor/serializer/protobuf.js
@@ -5,40 +5,30 @@ const protobuf = function () {
     return {
         serialize(payload) {
             logger.debug(`Protobuf.serialize()`);
-            return new Promise((resolve, reject) => {
-                protobufjs.load(`vendor/serializer/message.proto`)
-                    .then((root) => {
-                        const CrawlMessage = root.lookupType(`crawl.CrawlMessage`);
+            return protobufjs.load(`vendor/serializer/message.proto`)
+                .then((root) => {
+                    const CrawlMessage = root.lookupType(`crawl.CrawlMessage`);
 
-                        // Verify the payload if necessary (i.e. when possibly
-                        // incomplete or invalid)
-                        const errMsg = CrawlMessage.verify(payload);
-                        if (errMsg) { throw Error(errMsg); }
+                    // Verify the payload if necessary (i.e. when possibly
+                    // incomplete or invalid)
+                    const errMsg = CrawlMessage.verify(payload);
+                    if (errMsg) { throw Error(errMsg); }
 
-                        // Create a new message
-                        const rawMessage = CrawlMessage.create(payload);
-                        const buffer = CrawlMessage.encode(rawMessage).finish();
+                    // Create a new message
+                    const rawMessage = CrawlMessage.create(payload);
+                    const buffer = CrawlMessage.encode(rawMessage).finish();
 
-                        resolve(buffer);
-                    })
-                    .catch((error) => {
-                        reject(error);
-                    });
-            });
+                    return buffer;
+                })
         },
         deSerialize(buffer) {
             logger.debug(`Protobuf.deSerialize() - buffer size:${buffer.length}`);
-            return new Promise((resolve, reject) => {
-                protobufjs.load(`vendor/serializer/message.proto`)
-                    .then((root) => {
-                        const CrawlMessage = root.lookupType(`crawl.CrawlMessage`);
-                        const message = CrawlMessage.decode(buffer);
-                        resolve(message);
-                    })
-                    .catch((error) => {
-                        reject(error);
-                    });
-            });
+            return protobufjs.load(`vendor/serializer/message.proto`)
+                .then((root) => {
+                    const CrawlMessage = root.lookupType(`crawl.CrawlMessage`);
+                    const message = CrawlMessage.decode(buffer);
+                    return message;
+                });
         },
     };
 };
